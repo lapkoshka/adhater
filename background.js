42dd7703a2abc5da2932282075979c678d26f1e6
Lapko <lapkoshka@yandex.ru>
2018-01-16 01:20:18 +0300

    'update'

--- background.js
+++ background.js
@@ -12,7 +12,7 @@ chrome.tabs.onUpdated.addListener(function tabUpdateListener(id, status, info) {
     }
     const isVkPage = isVkTab(info.url) && status.status === 'complete';
     if (isVkPage) {
-        removeAdSidebar(id);
+        //removeAdSidebar(id);
 
         //Audio AD investigating
         if (info.audible && info.title === "Реклама") {
@@ -37,12 +37,17 @@ function removeAdPost(tabId) {
     chrome.tabs.executeScript(tabId, {file : "scripts/post.js"});
 };
 
-//Если получится блочить по урлу, то чекнуть можно ли эту штуку блочить
-//если запрос идет на один сервер, если на разные, то оставить так
 function removeAdSidebar(tabId) {
     chrome.tabs.executeScript(tabId, {file : "scripts/sidebar.js"});
 };
 
-// chrome.webRequest.onBeforeRequest.addListener(evt => {
-//     console.log(arguments);
-// })
+chrome.webRequest.onBeforeRequest.addListener(
+    details => {
+        console.log(details.url);
+        return {
+            redirectUrl: 'https://www.mail.ru/'
+        }
+    }, 
+    {'urls': ['*://ad.mail.ru/static/admanhtml/*', '*://vk.com/js/lib/px.js?ch=2']},  
+    ['blocking']
+);
