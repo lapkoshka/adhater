a4bc5f833b0064ecab3a31ac1a915facd89268c6
artem.lapko <artyom.lapko@ostrovok.ru>
2018-01-15 18:44:22 +0300

    update readme and remove comments

--- scripts/post.js
+++ scripts/post.js
@@ -60,14 +60,3 @@ document.addEventListener('scroll', evt => {
     adHater.lastScrollHeight = document.body.scrollHeight
     postSearcher.findAndRemove();
 });
-
-// var s = document.createElement ("script");
-// s.src = 'https://www.gstatic.com/firebasejs/3.6.9/firebase.js'
-// s.async = false;
-// document.documentElement.appendChild(s);
-// document.addEventListener('DOMContentLoaded', fireContentLoadedEvent, false);
-// // window.onload = fireContentLoadedEvent;
-
-// function fireContentLoadedEvent () {
-//     console.log (window.firebase);
-// }
