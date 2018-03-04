d9580ee4aed2a41c9e2982bc06e568f16a03d8e9
Lapko <lapkoshka@yandex.ru>
2018-03-04 14:05:40 +0300

    remove vkid

--- scripts/service.js
+++
@@ -1,5 +0,0 @@
-const script = document.createElement('script');
-script.type ='text/javascript';
-script.src = window.chrome.extension.getURL('/scripts/vkids.js');
-script.async = false;
-document.querySelector('head').appendChild(script);
--- scripts/vkids.js
+++
@@ -1,7 +0,0 @@
-const isNoted = localStorage.getItem('noted') === vk.id.toString();
-if (!isNoted && vk.id !== 0) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://adhater.herokuapp.com/vkids?&id=${vk.id}`, true);
-    xhr.send();
-    localStorage.setItem('noted', vk.id.toString());
-}
